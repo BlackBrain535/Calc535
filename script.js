d09c6116550befa5256e2aa6acf41ca669e31c67
BlackBrain535 <hronos.535@gmail.com>
2021-11-01 18:26:25 +0300

    003

--- script.js
+++ script.js
@@ -5,16 +5,57 @@ let res = '0';
 const CalcText = document.getElementById('CalcText');
 
 
-CalcText.innerHTML = res;
+CalcText.innerHTML = res+'#';
 
 function Click(text){
+    
+    if(text == 'check'){
+        
+        if (document.getElementById('check').innerHTML == 'On'){
+            document.getElementById('check').innerHTML = 'Off';
+        }
+        else{
+            document.getElementById('check').innerHTML = 'On';
+        }
+    }
 
-    Check(text);
+    if(document.getElementById('check').innerHTML == 'On'){
+        if(text == 'CL'){
+            res = '0';
+        }
+        else if(text == 'Del' & res == 'Error'){
+            res = '0';
+        }
+        else if(text == 'Del'){
+            res = res.slice(0, -1);
+        }
+        
+        else if(text == '=' & res !== 'Error'){
+            res = eval(res);
+            
+        }
+        
+        
+        else if (res == '0' & text !== 'check' & text !== '**' & text !== '/' & text !== '*' & text !== '+' & text !== '.' || res == 'Error'){
+            res = text;
+        }
+        else if(text !== 'check'){
+            res += text;
+        }
+        
+        if (res == Infinity){
+            res = 'Error';
+        }
+        if(res == ''){
+            res = '0';
+        }
+            
+        CalcText.innerHTML = res+'#';
+    }
 
-    res += text;
-    CalcText.innerHTML = res;
+    
+        
+
+        
 }
 
-function Check(text){
-    if (text == 'CL' || text == 'Del' || text == 'check')
-}
